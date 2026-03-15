6d9b14c13a814c0082cd72960f3d2081bf2d5f4f
Wiktor Trybus <wiktortrybus11@gmail.com>
2026-03-15 11:55:31 +0100

    Finished zad3.

--- lab1/zad3/child.c
+++ lab1/zad3/child.c
@@ -1,5 +1,6 @@
 #include "definitions.h"
 #include <string.h>
+#include <sys/file.h>
 
 int main(int argc, char *argv[]) {
 
@@ -22,15 +23,31 @@ int main(int argc, char *argv[]) {
         perror("ERROR: couldn't open file\n");
         return 1;
     }
+    int fd = fileno(file);
 
     char buf[32];
     snprintf(buf, sizeof(buf), "Potomek (%d)\n", getpid());
     size_t msg_length = strlen(buf);
 
+    // Setting a lock. Exclusive lock works in such way, that processes
+    // that encounter this lock will be put to sleep until the lock is released
+    if (flock(fd, LOCK_EX) == -1) {
+        perror("ERROR: Nie udało się założyć blokady\n");
+        fclose(file);
+        return 1;
+    }
+
     for (int i = 0; i < M; ++i) {
         fwrite(buf, sizeof(char), msg_length, file);
         fflush(file);
     }
+
+    // Releasing a lock
+    if (flock(fd, LOCK_UN) == -1) {
+        perror("ERROR: Nie udało się zdjąć blokady\n");
+        fclose(file);
+        return 1;
+    }   
 
     fclose(file);
 
