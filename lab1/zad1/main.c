30db59fc9bfa9b0114de970f95aca464b030308b
Wiktor Trybus <wiktortrybus11@gmail.com>
2026-03-15 10:00:16 +0100

    Added input parsing in zad1/.

--- lab1/zad1/main.c
+++ lab1/zad1/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+const int zmiennaGlobalna = 1;
 
-    printf("Hello\n");
+int main(int argc, char* argv[]) {
+
+    // PARSE INPUT
+    if (argc != 2) {
+        perror("ERROR: Nieprawidłowa liczba argumentów startowych.\n");
+        printf("Użycie programu: %s <liczba>\n", argv[0]);
+        return 1;
+    }
+    char *endptr;
+    const long N = strtol(argv[1], &endptr, 10);
+
+    if (endptr == argv[1] || *endptr != '\0') {
+        perror("ERROR: Nieprawidłowy input.\n");
+        printf("Użycie programu: %s <liczba>\n", argv[0]);
+    }
+
+    
+
     return 0;
 }
