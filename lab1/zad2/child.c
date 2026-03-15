0e01c8b5704e77df026be6532c5b7b1dc947a32b
Wiktor Trybus <wiktortrybus11@gmail.com>
2026-03-15 11:10:27 +0100

    Zad2 done

---
+++ lab1/zad2/child.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+    // Parse input
+    if (argc != 2) {
+        perror("ERROR: Nieprawidłowa liczba argumentów startowych.\n");
+        printf("Użycie programu: %s <liczba>\n", argv[0]);
+        return 1;
+    }
+    char *endptr;
+    const long M = strtol(argv[1], &endptr, 10);
+
+    if (endptr == argv[1] || *endptr != '\0') {
+        perror("ERROR: Nieprawidłowy input.\n");
+        printf("Użycie programu: %s <liczba>\n", argv[0]);
+    }
+
+    for (int i = 0; i < M; ++i) {
+        printf("Potomek (%d)\n", getpid());
+    }
+
+    return 0;
+}
