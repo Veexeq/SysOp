e1f839906fc0b26ee458630a1b6ba305f9cca3f5
Wiktor Trybus <wiktortrybus11@gmail.com>
2026-04-14 23:18:23 +0200

    Solved exercise 2.

---
+++ lab3/zad2/program2.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include "common.h"
+#include "../zad1/integrator.h" 
+
+int main() {
+    printf("--- PROGRAM 2 (Liczący) ---\n");
+
+    // Tworzenie potoków na dysku
+    if (mkfifo(FIFO_REQ, 0666) == -1 && errno != EEXIST) {
+        perror("Blad tworzenia FIFO_REQ\n");
+        return EXIT_FAILURE;
+    }
+    if (mkfifo(FIFO_RES, 0666) == -1 && errno != EEXIST) {
+        perror("Blad tworzenia FIFO_RES\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("[Program 2] Potoki gotowe. Wchodze w tryb nasluchiwania...\n");
+
+    // Nieskończona pętla - obsłuży wiele zleceń po kolei
+    while (1) {
+        printf("\n[Program 2] Czekam na zlecenia w FIFO_REQ...\n");
+        
+        // 1. Otwieramy potok TYLKO do odczytu
+        // Program zasypia w tym miejscu, aż Program 1 wpisze dane
+        int fd_req = open(FIFO_REQ, O_RDONLY);
+        if (fd_req == -1) {
+            perror("Blad otwarcia FIFO_REQ\n");
+            continue; // Zamiast ubijać proces, próbujemy ponownie w następnej pętli
+        }
+
+        // 2. Odbieramy dane
+        Interval req;
+        if (read(fd_req, &req, sizeof(Interval)) == sizeof(Interval)) {
+            printf("[Program 2] Otrzymano zlecenie: [%.2f, %.2f], dx = %.9f\n", 
+                   req.start, req.end, req.dx);
+            
+            // Zamykamy potok dla tego zlecenia
+            close(fd_req);
+
+            // 3. Rozpoczynamy obliczenia
+            printf("[Program 2] Trwaja obliczenia...\n");
+            double wynik = rectangular_integration(req.start, req.end, req.dx);
+            printf("[Program 2] Obliczenia zakonczone (wynik: %.9f). Odsylam...\n", wynik);
+
+            // 4. Otwieramy potok odpowiedzi tylko do zapisu
+            int fd_res = open(FIFO_RES, O_WRONLY);
+            if (fd_res != -1) {
+                write(fd_res, &wynik, sizeof(double));
+                close(fd_res);
+                printf("[Program 2] Wynik pomyslnie wyslany.\n");
+            } else {
+                perror("Blad otwarcia FIFO_RES do odeslania wyniku\n");
+            }
+        } else {
+            // Jeśli read zwróciło coś innego, to znaczy, że ktoś otworzył potok, 
+            // ale nic nie wysłał (np. przerwał program 1 w trakcie działania)
+            close(fd_req);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
